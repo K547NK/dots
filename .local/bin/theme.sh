2d8b5e966249b68db72c60c4f045c05770273a93
K547NK <nigel.agwah@gmail.com>
2022-04-01 15:31:11 -0500

    Theming complete

--- .local/bin/theme.sh
+++ .local/bin/theme.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-home=$(cat ~/.fehbg | cut -c 31- | sed "s/'/ /")
-wal -i ${home} -n
+home=$(cat ~/.fehbg | cut -c 31- | sed "s/'/ /") # Get the wallpaper info
+wal -i ${home} -n                                # Set wallpaper
